c82800f82fcfc35d2ad00a01649e99260907a4dd
Umair Meredia <60583253+umeredia@users.noreply.github.com>
2024-11-12 17:26:11 -0800

    Delete Palantir Top 5 Off Market Increases.sql

--- Palantir Top 5 Off Market Increases.sql
+++
@@ -1,8 +0,0 @@
-SELECT Date,
-	Open,
-    Close,
-    LAG(Close) OVER (Order BY Date) Previous_Day_Close,
-    Open -  LAG(Close) OVER (Order BY Date) Off_Hr_Changes
-FROM palantir_stock_price
-Order By Off_Hr_Changes DESC
-LIMIT 5
